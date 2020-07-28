8bbd506ab55b46afa4026dba7820a884d1a9f87f
electrob <aman.kashyap47@gmail.com>
2020-07-29 02:08:18 +0530

    Initial commit, faulty button test

---
+++ app/src/main/java/com/example/sanbox2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.sanbox2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.custom_button)
+        button?.setOnClickListener()
+        {
+            Toast.makeText(this@MainActivity,
+                R.string.app_name, Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
